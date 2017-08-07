c95e09987ef88aee942c77b2bab0c0e18cb2a9b4
jonaylor89 <jonaylor89@gmail.com>
2017-08-07 00:42:26 -0400

    Initial Commit

---
+++ GenerateGraph.py
@@ -0,0 +1,87 @@
+"""
+
+Author: John Naylor
+
+Description:
+    A window is created allowing
+    the user to enter the filename
+    to a csv file containing physics
+    linear acceleration data. With the
+    data, a line graph is generated
+    with x, y, z, and total acceleration.
+
+"""
+import sys
+import os
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from PyQt5 import QtWidgets, QtGui
+
+
+class Window(QtWidgets.QMainWindow):
+    """ Class for the initial window for user to enter a filename """
+
+    def __init__(self):
+        super(Window, self).__init__()
+
+        self.setGeometry(50, 50, 500, 300)
+        self.setWindowTitle('Data Analysis')
+        self.setWindowIcon(QtGui.QIcon('WindowIcon.png'))
+
+        self.btn = QtWidgets.QPushButton("Generate Graph", self)
+        self.btn.clicked.connect(self.button_handler)
+        self.btn.setAutoDefault(True)
+        self.btn.move(200, 150)
+
+        self.textbox = QtWidgets.QLineEdit(self)
+        self.textbox.move(110, 100)
+        self.textbox.resize(280, 40)
+
+        self.label = QtWidgets.QLabel(self)
+        self.label.setStyleSheet('color: red')
+
+        self.show()
+
+    def button_handler(self):
+        """ Checks to see if the filename is in the right directory """
+
+        textboxValue = self.textbox.text()
+
+        if os.path.isfile('%s.csv' % textboxValue):
+            self.label.setText("")
+            df = pd.read_csv(textboxValue + ".csv")
+            generate_graph(df)
+
+        else:
+            self.label.setText("File must be in the PhysicsAnalysis directory")
+            self.label.adjustSize()
+
+
+def generate_graph(data):
+    """ Plots x, y, z, and total acceleration versus time and shows graph """
+
+    plt.plot(data['time'], data['ax'], 'r-',
+             label="'x' Acceleration", alpha=0.7)
+    plt.plot(data['time'], data['ay'], 'b-',
+             label="'y' Acceleration", alpha=0.7)
+    plt.plot(data['time'], data['az'], 'g-',
+             label="'z' Acceleration", alpha=0.7)
+    plt.plot(data['time'], data['aT'], 'k-',
+             label="Total Acceleration", alpha=0.7)
+
+    plt.title("Acceleration vs Time")
+    plt.ylabel('Acceleration (m/s^2)')
+    plt.xlabel('Time (s)')
+
+    plt.legend(loc='upper left')
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    GUI = Window()
+
+    app.exec_()
---
+++ README.txt
@@ -0,0 +1,17 @@
+
+--------------Android-----------------
+1.) Change setting in the app to Linear Acceleration
+2.) To begin collecting data, press the large red button
+3.) To stop the collection, press the large red button again
+4.) Change the filename to the desired name and email to yourself
+---------------------------------------
+
+
+5.) Download the CSV file from your email onto your laptop
+6.) Move the file to the PhysicsAnalysis directory
+7.) Open command prompt and change into the PhysicsAnalysis directory
+8.) Run the command ~\PhysicsAnalysis>python GenerateGraph.py  
+9.) Into the text box, type the filename of your CSV file and press 'Generate Graph'
+
+
+
